126e9732f3df322ee4fad988e8d69be8887c2262
Oleksandr Zahorskyi <alex.alzag.zagorsky@gmail.com>
2023-04-09 17:05:04 +0300

    Create operations.js

---
+++ src/redux/operations.js
@@ -0,0 +1,43 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://6432aadcd0127730d2d72e6e.mockapi.io/phonebook";
+
+export const fetchContacts = createAsyncThunk(
+  "contacts/fetchAll",
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get("/contacts");
+      console.log(response.data)
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const addContact = createAsyncThunk(
+  "contacts/addContact",
+  async (contactInfo, thunkAPI) => {
+    try {
+      const response = await axios.post("/contacts", { ...contactInfo });
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  "contacts/deleteContact",
+  async (contactId, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/contacts/${contactId}`);
+
+      return response.data;
+    } catch (e) {
+      console.log(e.message)
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
